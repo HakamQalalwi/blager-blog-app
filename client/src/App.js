6f39e221649c7fc0facf9c74404babb1ab880ad4
Hakam Qalalwi <hakamqalalwi@outlook.com>
2023-11-29 22:45:00 +0300

    initialize react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,59 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Write from "./pages/Write";
+import Single from "./pages/Single";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
