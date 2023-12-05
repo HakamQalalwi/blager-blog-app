438984f9b4010f3389d38b0377cc56a3d176a868
Hakam Qalalwi <hakamqalalwi@outlook.com>
2023-12-05 22:49:17 +0300

    install required packages and fix error on login functionality

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -26,7 +26,7 @@ export const register = (req, res) => {
 export const login = (req, res) => {
   const q = "SELECT * FROM users WHERE username = ?";
 
-  db.query(q, [req, body, username], (err, data) => {
+  db.query(q, [req.body, username], (err, data) => {
     if (err) return res.json(err);
     if (data.length === 0) return res.status(404).json("User not found!");
 
