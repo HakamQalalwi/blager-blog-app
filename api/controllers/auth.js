f1b2ffd3ac394b6363ac8aa1093c8412cba01292
Hakam Qalalwi <hakamqalalwi@outlook.com>
2023-12-05 22:58:14 +0300

    use JWT to login

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import { db } from "../db.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   // Check existing user
@@ -26,7 +27,7 @@ export const register = (req, res) => {
 export const login = (req, res) => {
   const q = "SELECT * FROM users WHERE username = ?";
 
-  db.query(q, [req.body, username], (err, data) => {
+  db.query(q, [req.body.username], (err, data) => {
     if (err) return res.json(err);
     if (data.length === 0) return res.status(404).json("User not found!");
 
@@ -38,6 +39,17 @@ export const login = (req, res) => {
 
     if (!isPasswordCorrect)
       return res.status(400).json("Wrong username or password");
+
+    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+
+    const { password, ...other } = data[0];
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other);
   });
 };
 
