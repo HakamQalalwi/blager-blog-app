d0f191c90af12f8c45e157655ccd52908329788b
Hakam Qalalwi <hakamqalalwi@outlook.com>
2023-12-05 21:17:44 +0300

    route controller structure

--- api/db.js
+++ api/db.js
@@ -1,7 +1,8 @@
 import mysql, { createConnection } from "mysql"
 
-export const db = createConnection({
-  host:"localhost",
-  user:"root",
-  password:
-})
+export const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "hakam",
+  database: "blog",
+});
