65abcad2e5e359dc0ee50dc0c0724d94970d5fd5
Hakam Qalalwi <hakamqalalwi@outlook.com>
2023-12-06 21:34:53 +0300

    create , update and add posts functionalities

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -15,7 +15,7 @@ export const getPosts = (req, res) => {
 
 export const getPost = (req, res) => {
   const q =
-    "SELECT `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`, `data` FROM users u JOIN posts p ON u.id=p.id WHERE p.id = ? ";
+    "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`, `data` FROM users u JOIN posts p ON u.id=p.id WHERE p.id = ? ";
 
   db.query(q, [req.params.id], (err, data) => {
     if (err) return res.status(500).json(err);
@@ -25,7 +25,29 @@ export const getPost = (req, res) => {
 };
 
 export const addPost = (req, res) => {
-  res.json("from controller");
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Not authenticated!");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q =
+      "INSERT INTO posts `title`, `desc`, `img`, `cat`, `date`, `uid` VALUES ?";
+
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.img,
+      req.body.cat,
+      req.body.date,
+      userInfo.id,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json("Post has been created.");
+    });
+  });
 };
 
 export const deletePost = (req, res) => {
@@ -48,5 +70,20 @@ export const deletePost = (req, res) => {
 };
 
 export const updatePost = (req, res) => {
-  res.json("from controller");
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Not authenticated!");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+    const postId = req.params.id;
+    const q =
+      "UPDATE posts SET `title`=?, `desc`=?, `img`=?, `cat`=? WHERE `id` = ? and `uid` = ?";
+
+    const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
+
+    db.query(q, [...values, postId, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json("Post has been updated.");
+    });
+  });
 };
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -39,10 +39,15 @@ const Single = () => {
     }
   };
 
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    return doc.body.textContent;
+  };
+
   return (
     <div className="single">
       <div className="content">
-        <img src={post?.img} alt="" />
+        <img src={`../upload/${post?.img}`} alt="" />
         <div className="user">
           {post.userImg && <img src={post.userImg} alt="" />}
           <div className="info">
@@ -51,7 +56,7 @@ const Single = () => {
           </div>
           {currentUser.username === post.username && (
             <div className="edit">
-              <Link to={`/write?edit=2`}>
+              <Link to={`/write?edit=2`} state={post}>
                 <img src={Edit} alt="" />
               </Link>
               <img onClick={handleDelete} src={Delete} alt="" />
@@ -59,9 +64,9 @@ const Single = () => {
           )}
         </div>
         <h1>{post.title}</h1>
-        {post.desc}
+        {getText(post.desc)}
       </div>
-      <Menu />
+      <Menu cat={post.cat} />
     </div>
   );
 };
